a6cd4ac49bf0c54316efd0d9af60f49d70164632
E Stringham <backfrip@users.noreply.github.com>
2015-01-09 12:27:00 -0500

    Intermediate commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
