0e30977a0305ce0a12de5f3e0991b6936b8c0877
jeremyelkayam <jeremyelkayam@gmail.com>
2015-01-10 01:06:05 -0500

    Added some new graphics & sound and made a feeble attempt at implementing them

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,11 @@ public class Main {
 	    config.title = "Super Smash Bros. K";
 	    config.width = 1200;
 	    config.height = 720;
+	    config.x=-1;
+	    config.y=-1;
+	    config.foregroundFPS=60;
+	    config.backgroundFPS=0;
+	    config.addIcon("resource/16px-icon.png", Internal);//I DON'T KNOW IF THIS WORKS
 	    new LwjglApplication(new Smash(), config);
 	}
 }
