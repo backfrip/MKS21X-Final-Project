ce38b71ec908956bfcc846ba9f59e5f639ec6e36
E Stringham <backfrip@users.noreply.github.com>
2015-01-10 00:18:56 -0500

    Well I wanted to add some effect so I could tell that Splash was reloading, but I guess making it look nicer is okay.

--- src/screen/Splash.java
+++ src/screen/Splash.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -13,16 +14,22 @@ public class Splash implements Screen {
     private SpriteBatch batch;
     private Texture splash;
     private Smash game;
+    private OrthographicCamera camera;
 
     public Splash(Smash gameRef) {
 	game = gameRef;
+	camera = new OrthographicCamera();
+	camera.setToOrtho(false, 32, 18);
     }
 
     @Override
     public void render(float delta) {
+	Gdx.gl.glClearColor(0, 0, 0, 1);
 	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+	batch.setProjectionMatrix(camera.combined);
 	batch.begin();
-	batch.draw(splash, 0, 0);
+	batch.draw(splash, 1, 1, 30, 16);
 	batch.end();
 
 	if (Gdx.input.justTouched())
