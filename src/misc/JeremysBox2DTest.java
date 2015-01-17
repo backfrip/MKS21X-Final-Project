1a2e1f150fd4bdd3117f8605c96cb8294ea1439e
Jeremy Elkayam <jeremyelkayam@gmail.com>
2015-01-17 11:52:44 -0500

    Test continued

--- src/misc/JeremysBox2DTest.java
+++ src/misc/JeremysBox2DTest.java
@@ -1,109 +1,174 @@
 package misc;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.ChainShape;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.files.FileHandle;
 
 public class JeremysBox2DTest implements Screen {
-    
-    private World world;
-    private Box2DDebugRenderer debugRenderer;
-    private OrthographicCamera camera;
-    private Body bottleModel;
-    private Vector2 bottleModelOrigin;
-    private Sprite bottleSprite;
+	
+	private World world;
+	private Box2DDebugRenderer debugRenderer;
+	private OrthographicCamera camera;
+	
+	private final float TIMESTEP = 1 / 60f;
+	private final int VELOCITYITERATIONS = 8, POSITIONITERATIONS = 3;
+	
+	private float speed = 250;
+	private Vector2 movement = new Vector2();
+	private Body box;
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		world.step(TIMESTEP, VELOCITYITERATIONS, POSITIONITERATIONS);
+		box.applyForceToCenter(movement, true);
+		
+		camera.position.set(box.getPosition().x, box.getPosition().y, 0);
+		camera.update();
+		
+		debugRenderer.render(world, camera.combined);
+	}
 
-    private final float TIMESTEP = 1 / 60f;
-    private final int VELOCITYITERATIONS = 8, POSITIONITERATIONS = 3;
-    
-    @Override
-    public void render(float delta) {
-	Gdx.gl.glClearColor(0, 0, 0, 1);
-	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	
-	debugRenderer.render(world, camera.combined);
-	
-	world.step(TIMESTEP, VELOCITYITERATIONS, POSITIONITERATIONS);
-	Vector2 bottlePos = bottleModel.getPosition().sub(bottleModelOrigin);
-	bottleSprite.setPosition(bottlePos.x, bottlePos.y);
-	bottleSprite.setOrigin(bottleModelOrigin.x, bottleModelOrigin.y);
-	bottleSprite.setRotation(bottleModel.getAngle() * MathUtils.radiansToDegrees);
-    }
-    
-    @Override
-    public void resize(int width, int height) {
-    }
-    
-    @Override
-    public void show() {
-	world = new World(new Vector2(0, -9.81f), true);
-	debugRenderer = new Box2DDebugRenderer();
-	
-	camera = new OrthographicCamera(Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
-	
-	// body definiton
-	BodyDef ballDef = new BodyDef();
-	ballDef.type = BodyType.DynamicBody;
-	ballDef.position.set(0, 1);
-	
-	// ball shape
-	CircleShape shape = new CircleShape();
-	shape.setRadius(.5f);
-	
-	// fixture definition
-	FixtureDef fixtureDef = new FixtureDef();
-	fixtureDef.shape = shape;
-	fixtureDef.density = 2.5f;
-	fixtureDef.friction = .25f;
-	fixtureDef.restitution = .75f;
-	
-	world.createBody(ballDef).createFixture(fixtureDef);
-	
-	shape.dispose();
-	createBottle();
-    }
+	@Override
+	public void resize(int width, int height) {
+		camera.viewportWidth = width / 25;
+		camera.viewportHeight = height / 25;
+		camera.update();
+	}
 
-    @Override
+	@Override
+	public void show() {
+		world = new World(new Vector2(0, -9.81f), true);
+		debugRenderer = new Box2DDebugRenderer();
+		
+		camera = new OrthographicCamera();
+		
+		Gdx.input.setInputProcessor(new InputStuff() {
+			@Override
+			public boolean keyDown(int keycode) {
+				switch(keycode) {
+				case Keys.W:
+					movement.y = speed;
+					break;
+				case Keys.A:
+					movement.x = -speed;
+					break;
+				case Keys.S:
+					movement.y = -speed;
+					break;
+				case Keys.D:
+					movement.x = speed;
+				}
+				return true;
+			}
+			
+			@Override
+			public boolean keyUp(int keycode) {
+				switch(keycode) {
+				case Keys.W:
+				case Keys.S:
+					movement.y = 0;
+					break;
+				case Keys.A:
+				case Keys.D:
+					movement.x = 0;
+				}
+				return true;
+			}
+		});
+		
+		// BALL
+		// body definition
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyType.DynamicBody;
+		bodyDef.position.set(0, 10);
+		
+		// ball shape
+		CircleShape ballShape = new CircleShape();
+		ballShape.setRadius(.5f);
+		
+		// fixture definition
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = ballShape;
+		fixtureDef.density = 2.5f;
+		fixtureDef.friction = .25f;
+		fixtureDef.restitution = .75f;
+		
+		world.createBody(bodyDef).createFixture(fixtureDef);
+		
+		ballShape.dispose();
+		
+		// GROUND
+		// body definition
+		bodyDef.type = BodyType.StaticBody;
+		bodyDef.position.set(0, 0);
+		
+		// ground shape
+		ChainShape groundShape = new ChainShape();
+		groundShape.createChain(new Vector2[] {new Vector2(-50, 0), new Vector2(50, 0)});
+		
+		// fixture definition
+		fixtureDef.shape = groundShape;
+		fixtureDef.friction = .5f;
+		fixtureDef.restitution = 0;
+		
+		world.createBody(bodyDef).createFixture(fixtureDef);
+		
+		groundShape.dispose();
+		
+		// BOX
+		// body definition
+		bodyDef.type = BodyType.DynamicBody;
+		bodyDef.position.set(2.25f, 10);
+		
+		// box shape
+		PolygonShape boxShape = new PolygonShape();
+		boxShape.setAsBox(.5f, 1);
+		
+		// fixture definition
+		fixtureDef.shape = boxShape;
+		fixtureDef.friction = .75f;
+		fixtureDef.restitution = .1f;
+		fixtureDef.density = 5;
+		
+		box = world.createBody(bodyDef);
+		box.createFixture(fixtureDef);
+		
+		boxShape.dispose();
+	}
+
+	@Override
 	public void hide() {
-	dispose();
-    }
-    
-    @Override
-    public void pause() {
-    }
-    
-    @Override
+		dispose();
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
 	public void resume() {
-    }
-    
-    @Override
-    public void dispose() {
-	world.dispose();
-	debugRenderer.dispose();
-    }
-    private void createBottle() {
-	BodyEditorLoader loader = new BodyEditorLoader(new FileHandle("resource/test/test.json"));
-	BodyDef bd = new BodyDef();
-	bd.position.set(0, 0);
-	bd.type = BodyType.DynamicBody;
-	FixtureDef fd = new FixtureDef();
-	fd.density = 1;
-	fd.friction = 0.5f;
-	fd.restitution = 0.3f;
-	bottleModel = world.createBody(bd);
-	loader.attachFixture(bottleModel, "test01", fd, 1); 
-	bottleModelOrigin = loader.getOrigin("test01", 1).cpy();
-    }
+	}
+
+	@Override
+	public void dispose() {
+		world.dispose();
+		debugRenderer.dispose();
+	}
+
 }
