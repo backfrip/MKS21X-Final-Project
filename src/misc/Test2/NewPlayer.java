9eca9494a5866898ad6f01eb28d0aa816fc54c91
E Stringham <backfrip@users.noreply.github.com>
2015-01-20 23:58:51 -0500

    Last minute hitbox adding. Looks ugly but it's the best I can do.

--- src/misc/Test2/NewPlayer.java
+++ src/misc/Test2/NewPlayer.java
@@ -8,12 +8,19 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
+/**
+ * Template for what should actually be an abstract baseCharacter class that all
+ * the different characters derive from. Not yet included would be variables and
+ * methods handling various aspects to differentiate characters (dimensions,
+ * mass, friction, etc), as well as individualized attack boxes and attacks and
+ * whatnot.
+ */
 public class NewPlayer {
     private Body body;
-    private Fixture attack00, attack01;
+    private Fixture attack00, attack01, attack02, attack03, attack04;
     private Vector2 trajectory;
     private int aerialState; // 0 = ground; 1 = aerial; 2 = falling; 3 = falling
-			     // no actions
+			     // with no actions
 
     // @formatter:off
     // (Copied from PlayContactListener)
@@ -25,6 +32,18 @@ public class NewPlayer {
     // x   3   x
     // @formatter:on
 
+    /**
+     * Creates a new NewPlayer.
+     * 
+     * @param world
+     *            World in which the NewPlayer's body is created
+     * @param playerNum
+     *            Number used for NewPlayer's body's userData
+     * @param x
+     *            Height of NewPlayer
+     * @param y
+     *            Width of NewPlayer
+     */
     public NewPlayer(World world, int playerNum, float x, float y) {
 	BodyDef bd = new BodyDef();
 	bd.type = BodyDef.BodyType.DynamicBody;
@@ -65,6 +84,36 @@ public class NewPlayer {
 
 	attack01 = body.createFixture(fd);
 	attack01.setUserData("1");
+	
+	fd = new FixtureDef();
+	shape.setAsBox(.5f, 1.5f, new Vector2(1, 0), 0);
+	fd.shape = shape;
+	fd.isSensor = true;
+	fd.filter.categoryBits = Box2DTest2.ATTACK_SENSOR;
+	fd.filter.maskBits = Box2DTest2.PLAYER;
+
+	attack02 = body.createFixture(fd);
+	attack02.setUserData("2");
+
+	fd = new FixtureDef();
+	shape.setAsBox(1.5f, .5f, new Vector2(0, -1.5f), 0);
+	fd.shape = shape;
+	fd.isSensor = true;
+	fd.filter.categoryBits = Box2DTest2.ATTACK_SENSOR;
+	fd.filter.maskBits = Box2DTest2.PLAYER;
+
+	attack03 = body.createFixture(fd);
+	attack03.setUserData("3");
+	
+	fd = new FixtureDef();
+	shape.setAsBox(.5f, 1.5f, new Vector2(0, -1), 0);
+	fd.shape = shape;
+	fd.isSensor = true;
+	fd.filter.categoryBits = Box2DTest2.ATTACK_SENSOR;
+	fd.filter.maskBits = Box2DTest2.PLAYER;
+
+	attack04 = body.createFixture(fd);
+	attack04.setUserData("4");
 
 	shape.dispose();
 
